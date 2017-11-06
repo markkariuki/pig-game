e9baaa794d547df5657b2bae4113b09eef96c1b7
david mark <markkariuki17@gmail.com>
2017-11-06 22:36:36 +0300

    add change to js

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,5 @@ $(document).ready(function() {
     var playerOne = $("input#player1").val();
        var playerTwo = $("input#player2").val();
        //console.log(playerOne);
-
-               var newPlayerOne = new Player(playerOne);
-               var newPlayerTwo = new Player(playerTwo);
-               console.log(newPlayerOne);
-               console.log(newPlayerTwo);
-
-               players.push(newPlayerOne, newPlayerTwo);
-               $("h3#player1").text(players[0].playerName);
-               $("h3#player2").text(players[1].playerName);
-               $("div#player1-roll").show();
-               alert("Let's begin " + players[0].playerName);
-               $("form#players").fadeOut(3000);
-             });
-
-
-
-             $("button#die1").click(function() {
-                            var die = Math.floor(Math.random() * (6)) + 1;
-                            $("h2#die-count").text(die);
-
-                            if (die === 1) {
-                              $("div#player2-roll").fadeIn();
-                              $("div#player1-roll").fadeOut();
-
-hhgfgggcd
-})
+ var playerOne = $("input#player1").val();
+ var playerTwo = $("input#player2").val();
