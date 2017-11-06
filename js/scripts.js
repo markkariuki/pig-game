cc5368226d8e389e2df382a55dfec92c5e5c6a18
david mark <markkariuki17@gmail.com>
2017-11-06 22:46:28 +0300

    add the javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("div#player1").hide();
   $("div#player2-roll").hide();
-  $("form#players").submit(function(event) {
+  $("form#players").submit(function(event)); {
     event.preventDefault();
 
     var playerOne = $("input#player1").val();
@@ -9,3 +9,18 @@ $(document).ready(function() {
        //console.log(playerOne);
  var playerOne = $("input#player1").val();
  var playerTwo = $("input#player2").val();
+
+//console.log p1
+
+var newPlayerOne = new Player(playerOne);
+var newPlayerTwo = new Player(playerTwo);
+console.log(newPlayerOne);
+console.log (newPlayerTwo);
+
+players.push(newPlayerOne, newPlayerTwo);
+$("h3#player1").text(players[0].playerName);
+$("h3#player2").text([layers[1].playerName);
+$("div#player1-roll").show();
+ alert("may the game start" + players[0].playerName);
+ $("form#players").fadeOut(3000);)
+ )
